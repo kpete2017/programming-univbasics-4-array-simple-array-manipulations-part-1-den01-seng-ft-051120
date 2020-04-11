177e8d488f19276a313cc73811612858c2176145
Kyle Petersen <kpete2017@gmail.com>
2020-04-11 02:08:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,18 @@
 def using_push(array, string)
   array << string
 end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop
+  array.pop
+end
+
+def using_
